f2a77c92bd4b2d9408d745b133099410ebfb23db
Annafi <islamannafi@gmail.com>
2026-03-01 19:20:37 -0500

    Added database url to config and completed the database.py file

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_name: str = "FiscalCore"
     debug: bool = False
     api_prefix: str = "/api/v1"
+    database_url: str 
 
     class Config:
         env_file = ".env"
