fe13d6b637d8551c68beb58d6acb9da0e9bb0ed3
Paradox456 <islamannafi@gmail.com>
2026-03-14 00:11:27 -0400

    Added and edited some files

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -11,10 +11,10 @@ access_token_expire_minutes = settings.access_token_expire_minutes
 
 
 def hash_password(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
